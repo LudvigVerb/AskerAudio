330b085331772263b297ff46fbb57777d7e94099
NikolaiDokken <nikolaidokken@live.no>
2020-07-29 00:53:55 +0200

    router bug

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import "./index.css";
 import App from "./App";
 import About from "./views/About";
 import { BrowserRouter, Route } from "react-router-dom";
+console.log(process.env.PUBLIC_URL);
 
 ReactDOM.render(
   <BrowserRouter>
-    <Route exact path={"/"} component={App} />
-    <Route exact path={"/omOss"} component={About} />
+    <Route exact path={process.env.PUBLIC_URL + "/"} component={App} />
+    <Route exact path={process.env.PUBLIC_URL + "/omOss"} component={About} />
   </BrowserRouter>,
   document.getElementById("root")
 );
