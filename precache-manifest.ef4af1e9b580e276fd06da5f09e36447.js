409fa4183176f3246dbb06e580cd716e4351bd2e
LudvigVerb <68926503+LudvigVerb@users.noreply.github.com>
2020-07-28 21:53:07 +0000

    Deploying to gh-pages from  @ 3ca55302f2a8bdc3c4857f5f8c97355b9d83e33d 🚀

---
+++ precache-manifest.ef4af1e9b580e276fd06da5f09e36447.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cbf4d761582b5f82522a49a09fc740b",
+    "url": "/AskerAudio/index.html"
+  },
+  {
+    "revision": "a6b4e06b6e4efe0328ba",
+    "url": "/AskerAudio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a4b2151216f5184cbc8d",
+    "url": "/AskerAudio/static/js/2.783e867b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AskerAudio/static/js/2.783e867b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6b4e06b6e4efe0328ba",
+    "url": "/AskerAudio/static/js/main.9df79d01.chunk.js"
+  },
+  {
+    "revision": "334f0f8fe65d16d5c885",
+    "url": "/AskerAudio/static/js/runtime-main.6409976b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AskerAudio/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e23ef259085afbc004bd1dab4cf14621.js"
+  "/AskerAudio/precache-manifest.ef4af1e9b580e276fd06da5f09e36447.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AskerAudio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
